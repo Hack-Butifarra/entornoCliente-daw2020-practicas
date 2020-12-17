ebcdf96299f31b5004c8cffe69b0d43b67cc3664
Hack-Butifarra <amascarell28@gmail.com>
2020-12-17 17:11:52 +0100

    Pantalla Carga

--- Proyecto_Vainilla/js/script1.js
+++ Proyecto_Vainilla/js/script1.js
@@ -1,24 +1,21 @@
 'use strict'
 
-let timer;
 chargingScreen()
 
-function chargingScreen(opcion) {
+function chargingScreen() {
     const tiempo = 5000; // tiempo en ms
-    window.addEventListener('keyup', changeContent, false);
-
-    timer = setTimeout(() => {
-        window.removeEventListener('keyup', chargingScreen, false);
+    let timer = setTimeout(() => {
+        window.removeEventListener('keyup',myEvent,false);
         start();
     }, tiempo);
     
+    let myEvent = window.addEventListener('keyup', () => {
+        let event = window.event;
+        if (event.ctrlKey && event.keyCode == 121) {
+            clearTimeout(timer);
+            start();
+        }
+    }, false);
 }
 
 function start() { document.write('hola'); } // test
-
-function changeContent(event) { 
-    if (event.ctrlKey && event.keyCode == 121) {
-        clearTimeout(timer);
-        start();
-    }
-}
