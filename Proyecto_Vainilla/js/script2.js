08a7c4865575fe565ec2d72df4b0139497350ab1
Hack Butifarra <amascarell28@gmail.com>
2021-01-08 02:39:50 +0100

    Pantalla 2

--- Proyecto_Vainilla/js/script2.js
+++ Proyecto_Vainilla/js/script2.js
@@ -1,33 +1,33 @@
 'use strict'
- 
-accountInformation();
 
-function accountInformation() {
-    var email = search('email');
-    var fecha = search('fecha');
-    var hora = search('hora');
+var email = search('email');
+var fecha = search('fecha');
+var hora = search('hora');
 
-    var elementoPadre = document.getElementsByTagName('main')[0];
-    var parrafo1 = document.createElement('p');
-    var parrafo2 = document.createElement('p');
-    parrafo1.innerHTML = 'Hola' + email;
-    parrafo2.innerHTML = 'La ultima vez que entraste fue' + fecha + 'a las' + hora;
-    elementoPadre.appendChild(parrafo1);
-    elementoPadre.appendChild(parrafo2);
-}
+var elementoPadre = document.getElementsByTagName('main')[0];
+var parrafo1 = document.createElement('p');
+var parrafo2 = document.createElement('p');
+parrafo1.innerHTML = 'Hola ' + email;
+parrafo2.innerHTML = 'La ultima vez que entraste fue ' + fecha + ' a las ' + hora;
+elementoPadre.appendChild(parrafo1,parrafo2);
+elementoPadre.appendChild(parrafo2);
+
+window.addEventListener('click', () => {
+    window.location.href = "pantalla3.html";
+}, false);
 
 function search(campo) {
-    let valor;
+    let dato;
     let listaCookies = document.cookie.split(';');
 
-  // busca el nombre de la cookie (contador) y nos muestra su valor
+  // busca el nombre de la cookie y almacena su valor
     for (let cookie of listaCookies) {
         let [nombre, valor] = cookie.split('=');
-        if (nombre == campo) {
-            valor = nombre;
+        if (nombre.trim() == campo) { // trim() para eliminar los espacios en blanco y hacer coincidir el nombre con el valor correspondiente
+            dato = valor;
             break;
         }
     }
 
-    return valor;
+    return dato;
 }
